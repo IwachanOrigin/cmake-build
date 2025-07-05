579213ed36d58322d43cab19f5e2a05056ab7111
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2025-07-05 13:16:03 +0900

    [utils] Updated. Added license and copyright. Changed function name. Added function description.

--- cmake-utils.el
+++ cmake-utils.el
@@ -1,58 +1,63 @@
-;;; cmake-utils.el --- CMake 補助関数 -- lexical-binding t; --
+;;; cmake-utils.el --- Define functions to assist cmake-build.el -*- lexical-binding: t; -*-
 
-;; 使い方
-;; (myfind-topmost-cmake) を呼び出すと，現在バッファのファイルを起点に
-;;   1. `.git` が存在するディレクトリを“プロジェクト・ルート”とみなす
-;;   2. ルートから現在ファイルまでの階層を走査し
-;;   3. 最上位（＝ルートに最も近い）CMakeLists.txt を返す
-;; 見つからなければ nil を返す
+;; Copyright (C) 2025 Yuji Iwanaga
+
+;; Author: Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
+;; URL: https://github.com/IwachanOrigin/cmake-build
+;; Version: 1.0.0
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; How to Use
+;; Called cmake-utils-find-topmost-cmake-file,
+;; the following processing is performed starting from the file in the current buffer.
+;;   1. The directory where `git` is located is considered the "project root".
+;;   2. Traverses the hierarchy from the root to the current file.
+;;   3. Return the top-level (i.e., closest to the root) CMakeLists.txt full-path.
+;;   4. Return nil if not found
+
+;;; Code:
 
 (require 'cl-lib)
 
-(defun myfind-topmost-cmake (&optional buffer-file)
-  ""
+(defun cmake-utils-find-topmost-cmake-file (&optional buffer-file)
+  "Return the top-level (i.e., closest to the root) CMakeLists.txt full-path."
   (let* ((file (or buffer-file (buffer-file-name)))
-         ;; 1) Git ルートを決定
+         ;; 1) Determine Git Root
          (git-root (and file (locate-dominating-file file ".git")))
          (result   nil))
     (when git-root
-      ;; 2) ルート→ファイルのディレクトリ階層を列挙
+      ;; 2) Root -> Enumerate directory hierarchy of files
       (cl-loop
        with dir = (file-name-directory file)
        while (and dir (file-in-directory-p dir git-root))
        do (when (file-exists-p (expand-file-name "CMakeLists.txt" dir))
-            ;; 3) ルートに近いものほど後に来るため、毎回上書き
+            ;; 3) The closer to the root, the later it comes, so overwrite each time
             (setq result (expand-file-name "CMakeLists.txt" dir)))
-       ;; 親ディレクトリへ
+       ;; Go to parent direcoty
        do (setq dir (if (equal dir git-root)
                         nil
                       (file-name-directory (directory-file-name dir))))))
     result))
 
-(defun myfind-topmost-cmake-dir (&optional buffer-file)
-  ""
-  (let ((cmake-file (myfind-topmost-cmake buffer-file)))
+(defun cmake-utils-find-topmost-cmake-file-dir (&optional buffer-file)
+  "Return the top-level (i.e., closest to the root) CMakeLists.txt directory path."
+  (let ((cmake-file (cmake-utils-find-topmost-cmake-file buffer-file)))
     (when cmake-file
       (file-name-directory cmake-file))))
-
-;;; 簡易テスト (ert)
-(eval-when-compile (require 'ert))
-
-(ert-deftest myfind-topmost-cmake-test ()
-  (let ((tmp (make-temp-file cmake-test- t))
-        (root (expand-file-name repo tmp))
-        (src  (expand-file-name src  root))
-        (deep (expand-file-name deep src))
-        (file (expand-file-name main.cpp deep)))
-    (dolist (d (list root src deep)) (make-directory d t))
-    ;; ルートに .git, src と deep に CMakeLists.txt を作成
-    (make-directory (expand-file-name .git root) t)
-    (dolist (d (list src deep))
-      (with-temp-file (expand-file-name CMakeLists.txt d)))
-    (with-temp-file file) ; ダミー cpp
-
-    (should (equal (myfind-topmost-cmake file)
-                   (expand-file-name CMakeLists.txt src)))))
 
 (provide 'cmake-utils)
 ;;; cmake-utils.el ends here
