afe176b2777673a4044a01a45fd4d9f3fcfae1cc
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2025-07-07 11:07:10 +0900

    [build] Removed package-requires.

--- cmake-build.el
+++ cmake-build.el
@@ -5,7 +5,6 @@
 ;; Author: Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
 ;; URL: https://github.com/IwachanOrigin/cmake-build
 ;; Version: 1.0.0
-;; Package-Requires: ((cmake-utils "1.0.0"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
